39c5bfc1d685fd1245b4e6faa1d93c26dd8a3824
Sokha Sim <sokha.sim@student.passerellesnumeriques.org>
2025-01-22 10:36:29 +0700

    update responsive

--- js/home.js
+++ js/home.js
@@ -15,14 +15,50 @@ allSideMenu.forEach((item) => {
   });
 });
 
+
+
 // TOGGLE SIDEBAR
 const menuBar = document.querySelector("#content nav .bx.bx-menu");
 const sidebar = document.getElementById("sidebar");
-const imglogo = document.querySelector('img');
+const logo= document.querySelector("img");
 
 menuBar.addEventListener("click", function () {
   sidebar.classList.toggle("hide");
-  imglogo.classList.toggle("hide");
+  logo.classList.toggle("hide");
+});
+
+const searchButton = document.querySelector(
+  "#content nav form .form-input button"
+);
+const searchButtonIcon = document.querySelector(
+  "#content nav form .form-input button .bx"
+);
+const searchForm = document.querySelector("#content nav form");
+
+searchButton.addEventListener("click", function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault();
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchButtonIcon.classList.replace("bx-search", "bx-x");
+    } else {
+      searchButtonIcon.classList.replace("bx-x", "bx-search");
+    }
+  }
+});
+
+if (window.innerWidth < 768) {
+  sidebar.classList.add("hide");
+} else if (window.innerWidth > 576) {
+  searchButtonIcon.classList.replace("bx-x", "bx-search");
+  searchForm.classList.remove("show");
+}
+
+window.addEventListener("resize", function () {
+  if (this.innerWidth > 576) {
+    searchButtonIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
+  }
 });
 
 const switchMode = document.getElementById("switch-mode");
@@ -34,6 +70,9 @@ switchMode.addEventListener("change", function () {
     document.body.classList.remove("dark");
   }
 });
+
+
+
 // get date
 const startDate = document.querySelector(".text-1");
 const currentDate = new Date();
