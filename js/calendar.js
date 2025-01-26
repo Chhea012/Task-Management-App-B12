90dcbf60d05b39645f4722aeeeedc256bdfb55c8
Chhea012 <chhea.chhouy@student.passerellesnumeriques.org>
2025-01-26 12:18:48 +0700

    update

--- js/calendar.js
+++ js/calendar.js
@@ -354,3 +354,24 @@ document.head.appendChild(style);
 
 
 
+function confirmLogout() {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You will log out to back page.",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, Logout!',
+    cancelButtonText: 'Cancel'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Redirect to ../index.html
+      window.location.href = '../index.html';
+    }
+  });
+}
+
+
+
+
