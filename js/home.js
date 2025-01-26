91bf6c7a5ed09ac808b05a37b13c0350c1d40c34
Liyaan66 <liya.an@student.passerellesnumeriques.org>
2025-01-26 13:19:38 +0700

    update

--- js/home.js
+++ js/home.js
@@ -70,41 +70,6 @@ switchMode.addEventListener("change", function () {
     document.body.classList.remove("dark");
   }
 });
-<<<<<<< HEAD
-// get date
-const startDate = document.querySelector(".text-1");
-const currentDate = new Date();
-const year = currentDate.getFullYear();
-const month = currentDate.getMonth() + 1;
-const day = currentDate.getDate();
-const h3 = document.createElement("h3");
-h3.textContent = `${day}/${month}/${year}`;
-const paragraph = document.createElement("p");
-startDate.appendChild(h3);
-paragraph.textContent = "Today";
-startDate.appendChild(paragraph);
-// sum number
-
-// let allValues = document.querySelectorAll(".value");
-
-// allValues.forEach((singleValue) => {
-//   let startValue = 0;
-//   let endValue = parseInt(singleValue.getAttribute("data-value"));
-//   let duration = Math.floor(2000 / endValue);
-
-//   let counter = setInterval(function () {
-//     startValue += 1;
-//     singleValue.textContent = startValue;
-
-//     if (startValue == endValue) {
-//       clearInterval(counter);
-//     }
-//   }, duration);
-// });
-// Task statistics summary: Completed vs Pending Tasks
-
-// Task statistics summary: Completed vs Pending Tasks
-=======
 function confirmLogout() {
   Swal.fire({
     title: 'Are you sure?',
@@ -125,5 +90,4 @@ function confirmLogout() {
 
 
 
->>>>>>> 51e7bb5087678bf93c8c0966034747880a6b494b
 
